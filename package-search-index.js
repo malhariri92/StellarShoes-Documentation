764f60ddbbe3ebc8aa0fc9bf7e53494f9872f6f3
malhariri92 <malhariri@student.cscc.edu>
2020-08-04 08:48:34 -0400

    Initial commit

---
+++ package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"com.StellarShoes"},{"l":"com.StellarShoes.Beans"},{"l":"com.StellarShoes.Tests"},{"l":"com.StellarShoes.utils"}]
